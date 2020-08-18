8ea816bd8700d07b013ac2918981d4d1df0160ae
Yury Ognev <garonini1@gmail.com>
2020-08-18 18:04:56 +0700

    подготовил рабочее пространство для разработки

---
+++ src/components/app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css'
+
+const App = () => {
+    return (
+      <div>
+        <h1>Avito technical task</h1>
+      </div>
+    );
+    
+  }
+
+export default App;
